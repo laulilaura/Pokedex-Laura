1e4b5977c05ac7931476dfac9453942270292b3c
Laura Benaiton <laurabenaiton@gmail.com>
2022-02-22 20:14:57 +0100

    Init visu

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Pokédex',
+            company: 'The Pokémon Company',
+            endText: '©2022 Pokémon. ©1995-2022 Nintendo/Creatures Inc./GAME FREAK inc. TM,®Nintendo.'
+        }
+    }
+})
